4ecda64abb91c3b83dd268d6f022cec3dcf7eee9
inmanjm <inmanjm@gmail.com>
2017-09-19 10:59:33 -0400

    MODIFIED: make_BSR_trees.sh - removed phyllip calls, now uses make_trees.R for UPGMA and Neighbor-joining trees.

--- pangenome/bin/make_BSR_trees.sh
+++ pangenome/bin/make_BSR_trees.sh
@@ -13,53 +13,46 @@ results_dir="$base_dir/results"
 BSR_trees_dir="$results_dir/BSR_trees"
 core_trees_dir="$BSR_trees_dir/core_trees"
 all_trees_dir="$BSR_trees_dir/all_trees"
-cdm="100_pairwise_BSR_distance_matrix_phylip.txt"
-adm="0_pairwise_BSR_distance_matrix_phylip.txt"
+cdm="100_pairwise_BSR_distance_matrix.txt"
+adm="0_pairwise_BSR_distance_matrix.txt"
 # Will want to change srcdir to the pan-genome bin directory
 src_dir=$2
 
 # declare function create_dir that first checks for the presence of a directory, then creates it if absent
 
 function create_dir {
-   name=$1
-   if [ ! -d $name ]
-     then
-       echo " $name is not present, generating ..."
-       mkdir $name
-   fi
+    name=$1
+    if [ ! -d $name ]
+        then
+        echo " $name is not present, generating ..."
+        mkdir $name
+    fi
 }
 
 # delcare function make_trees to make infile, generate tree and make a pdf file
 
 function make_trees {
-   file=$1 # file is the name of the appropriate BSR distance matrix
-   name=$2 # second variable passed is "core" or "all"
+    file=$1 # file is the name of the appropriate BSR distance matrix
+    name=$2 # second variable passed is "core" or "all"
 
- # make the required Phylip "infile" from the appropriate BSR distance matrix
-   echo " creating Phylip infile from $file ..."
-   cat $results_dir/$file | perl -ne 'chomp; @a=split(/\t/); $line = join("  ", @a); print "$line\n";' > infile
-
- # generate a UPGMA tree using the Phylip program neighbor
-   echo " generating UPGMA $name tree ..."
-   $src_dir/UPGMA_tree_batch
+ # generate a UPGMA tree
+    echo " generating UPGMA $name tree ..."
+    $src_dir/make_trees.R $results_dir/$file average
 
  # rename the UPGMA output files
-   mv outfile outfile\_$name\_panBSR_UPGMA
-   mv outtree outtree\_$name\_panBSR_UPGMA.tre
+    mv outtree outtree\_$name\_panBSR_UPGMA.tre
 
  # generate Neighbor-Joining tree
-   echo " generating Neighbor-Joining $name tree ..."
-   $src_dir/neighbor_joining_tree_batch
+    echo " generating Neighbor-Joining $name tree ..."
+    $src_dir/make_trees.R $results_dir/$file nj
 
  # rename the Neighbor-Joining output files
-   mv outfile outfile\_$name\_panBSR_NJ
-   mv outtree outtree\_$name\_panBSR_NJ.tre
+    mv outtree outtree\_$name\_panBSR_NJ.tre
 
  # to make a pretty picture of the tree files (i.e., .pdf files)
- # you will want to move this to the src directory for the pan-genome pipeline
-   echo " making .pdf files ..."
-   ruby -I $src_dir/../lib/ruby $src_dir/Newick-ruby/newickDraw outtree\_$name\_panBSR_UPGMA.tre
-   ruby -I $src_dir/../lib/ruby $src_dir/Newick-ruby/newickDraw outtree\_$name\_panBSR_NJ.tre
+    echo " making .pdf files ..."
+    ruby -I $src_dir/../lib/ruby $src_dir/Newick-ruby/newickDraw outtree\_$name\_panBSR_UPGMA.tre
+    ruby -I $src_dir/../lib/ruby $src_dir/Newick-ruby/newickDraw outtree\_$name\_panBSR_NJ.tre
 }
 
 # Check for presence of BSR_trees directory. If not, create it
