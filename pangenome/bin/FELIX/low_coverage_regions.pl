32d07a9dd635077522a3ef2123bd5ac9ff088205
Granger Sutton <gsutton@jcvi.org>
2021-02-10 09:18:44 -0500

    add new program, low_coverage_regions.pl to find low coverage regions after pilon polishing

--- pangenome/bin/FELIX/low_coverage_regions.pl
+++ pangenome/bin/FELIX/low_coverage_regions.pl
@@ -11,15 +11,13 @@ use Carp;
 use strict;
 use warnings;
 
-my $help_text = "This program reads in a 3 column tab delimitted file from the coverage option file. The 3 columns are the contig name, the contig position, and the coverage depth - missing contig positions are assumed to have coverage 0. This program outputs to standard output a 3 column tab delimitted file: column 1 is contig name, column 2 is start of 0 coverage region, and column 3 is stop of 0 coverage region - final row for a contig will have a stop of end representing the end coordinate of the contig which is unknown to the program.
+my $help_text = "This program reads in a 3 column tab delimitted file from standard input. The 3 columns are the contig name, the contig position, and the coverage depth - missing contig positions are assumed to have coverage 0. This program outputs to standard output a 3 column tab delimitted file: column 1 is contig name, column 2 is start of 0 coverage region, and column 3 is stop of 0 coverage region - final row for a contig will have a stop of end representing the end coordinate of the contig which is unknown to the program.
 
 Input Flags:
 -threshold - used for column 3 to see if two genomes should be clustered <= (default 0.02)
--coverage - file name of coverage data
 -help - outputs this help text";
 
 GetOptions('threshold=s' => \my $threshold,
-	   'coverage=s' => \my $coverage_file,
 	   'help' => \my $help);
 	
 if(!$threshold) {
@@ -36,12 +34,8 @@ my $last_high = 0; # position of last high coverage base pair
 my $current_contig = ""; # current contig name
 
 #####################################################################################################
-my $coverage_handle;
-unless (open ($coverage_handle, "<", $coverage_file) )  {
-    die ("cannot open file $coverage_file!\n");
-}
 my $line;
-while ($line = <$coverage_handle>) {
+while ($line = <STDIN>) {
     (my $contig, my $position, my $coverage) = split(/\t/, $line, 3);
     if ($contig ne $current_contig) {
 	if ($current_contig ne "") {
@@ -64,4 +58,3 @@ if ($current_contig ne "") {
     $last_high++;
     print "$current_contig\t$last_high\tend\n";
 }
-close($coverage_handle);
